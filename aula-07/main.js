355fead289e73ee28a118cac1027726153aeb353
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-28 17:53:38 -0300

    Lesson 07: SetTimeout()

---
+++ aula-07/main.js
@@ -0,0 +1,9 @@
+/* 
+  SetTimeout()
+  - Executa uma função após um 
+  intervalo de tempo especificado. 
+*/
+
+setTimeout(() => {
+  console.log('Olá, tudo bem?');
+}, 3000); // tempo em milissegundos
