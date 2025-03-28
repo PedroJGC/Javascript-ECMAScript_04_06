a68f373f746b420f87149229e66d31c2c9e282a2
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-28 17:14:24 -0300

    Lesson 05: Rename imports

---
+++ aula-05/main.js
@@ -0,0 +1,8 @@
+/* 
+  Renomeando as importações
+*/
+
+import { sum as s, multiply as m } from '../aula-05/calc.js';
+
+console.log(`A soma de 4 + 6 = ${s(4, 6)}`);
+console.log(`A multiplicação de 4 * 6 = ${m(4, 6)}`);
