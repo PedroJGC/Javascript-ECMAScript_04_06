dc9e5515c0b2b57cf4fc5c9b4924ba2a4ff237f9
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-31 19:06:06 -0300

    Lesson 12: Packages - Using day js through the browser

---
+++ aula-12/main.js
@@ -0,0 +1,8 @@
+/* 
+  Pacotes
+   |> Utilizando o day js pelo browser
+   |> <script src="https://cdn.jsdelivr.net/npm/dayjs@1/dayjs.min.js"></script>
+*/
+
+const now = dayjs();
+console.log(now.format('DD/MM - HH:mm'));
