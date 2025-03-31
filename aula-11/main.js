3a9949d04c8225d2409567c28b8b7c6e694e2a13
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-31 18:46:42 -0300

    Lesson 11: Priorities and order of execution

---
+++ aula-11/main.js
@@ -0,0 +1,24 @@
+/* 
+  Prioridades e ordem de execução
+*/
+
+// (1) Executa o código de forma síncrona e o valor 1 é impresso imediatamente no console.
+console.log(1);
+
+// (3) Microtasks são executadas antes de temporizadores e promessas.
+queueMicrotask(() => {
+  console.log(2);
+});
+
+// (5) Macrotask que aguarda o evento de temporizador ser acionado.
+setTimeout(() => {
+  console.log(3);
+}, 1000);
+
+// (2) Execução síncrona.
+console.log(4);
+
+// (4) Adiciona uma microtask.
+Promise.resolve(true).then(() => {
+  console.log(5);
+});
